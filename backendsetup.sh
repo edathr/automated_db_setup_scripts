d8ad09976cba9e316428106b6feb23c37d3a2f4a
Eda <edathr2@gmail.com>
2019-12-08 20:58:34 +0800

    Changed pip to pip3

--- backendsetup.sh
+++ backendsetup.sh
@@ -3,11 +3,11 @@ sudo apt update
 git clone https://github.com/edathr/backend
 cd backend
 sudo rm -R env
-sudo apt install python-pip -y 
-sudo pip install virtualenv
+sudo apt install python3-pip -y 
+sudo pip3 install virtualenv
 sudo apt install python3-flask -y
 virtualenv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 export FLASK_APP=run.py
 flask run
