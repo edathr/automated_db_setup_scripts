f8f7e4435ae4ea1d1f80ae9062de656e710bcca7
Eda <edathr2@gmail.com>
2019-12-21 19:01:03 +0800

    Changed repo URL for frontend

--- frontendsetup.sh
+++ frontendsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/FavebookSUTD/favebook_frontend
-cd favebook_frontend
+git clone https://github.com/edathr/aoogebra_frontend
+cd aoogebra_frontend
 sudo apt install curl -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt install nodejs -y
