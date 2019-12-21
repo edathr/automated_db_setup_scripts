4128c198266ad5f13039b63a3dfebace3bfe6f87
Eda <edathr2@gmail.com>
2019-12-21 22:36:10 +0800

    Removed curl command

--- frontendadminsetup.sh
+++ frontendadminsetup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/FavebookSUTD/favebook_frontend_admin
 cd favebook_frontend_admin
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 npm install
 npm start
