e1ad134897a2045a1e7d5f9ef14b8dca8e4b6a88
Eda <edathr2@gmail.com>
2019-12-21 20:11:56 +0800

    Removed apt commands from backend and frontend scripts

--- backendsetup.sh
+++ backendsetup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-sudo apt update
 git clone https://github.com/edathr/backend
 cd backend
 sudo rm -R env
-sudo apt install python3-pip -y 
-sudo pip3 install virtualenv
-sudo apt install python3-flask -y
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
--- frontendsetup.sh
+++ frontendsetup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sudo apt update
 git clone https://github.com/edathr/aoogebra_frontend
 cd aoogebra_frontend
-sudo apt install curl -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs -y
 npm install
 npm start
