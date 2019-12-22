02f0f37361b83046f3f0abbfe19a9745b3c185a7
Eda <edathr2@gmail.com>
2019-12-22 16:44:38 +0800

    Change of file type, removed kindle-reviews.zip to new url and new file

--- mysqlseeding.sh
+++ mysqlseeding.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/kindle-reviews.zip -O kindle-reviews.zip
-sudo apt install unzip -y 
-unzip kindle-reviews.zip
-rm -rf kindle_reviews.json
-rm -rf *.zip
+wget -c https://aoogebradatabasesetup.s3-ap-southeast-1.amazonaws.com/kindle_reviews_correct_schema.csv -O kindle_reviews_correct_schema.csv
 sudo mysql -e "DROP database if exists 50043db;CREATE database 50043db;"
 wget -c https://raw.githubusercontent.com/edathr/automated_db_setup_scripts/master/setupsql.sql -O setupsql.sql
 sudo mysql -u root -b 50043db < setupsql.sql
