5783204331ae87114524c5cc736b043d1ac66344
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2019-12-22 18:08:39 +0800

    set up sql

--- setupsql.sql
+++ setupsql.sql
@@ -21,3 +21,44 @@ INTO TABLE historical_reviews
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+CREATE TABLE `revoked_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jti` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password_hash` varchar(120) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `ix_users_username` (`username`)
+);
+
+
+CREATE TABLE `favourites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) DEFAULT NULL,
+  `asin` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `asin_username_constraint` (`asin`,`username`),
+  KEY `username` (`username`),
+  CONSTRAINT `favourites_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+);
+
+CREATE TABLE `live_reviews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) DEFAULT NULL,
+  `asin` varchar(20) NOT NULL,
+  `review_rating` float DEFAULT NULL,
+  `review_text` text,
+  `unix_timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `asin_username_constraint` (`asin`,`username`),
+  KEY `username` (`username`),
+  CONSTRAINT `live_reviews_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+);
+
